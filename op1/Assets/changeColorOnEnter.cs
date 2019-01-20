ad60ddad7d9905c4c59e501ddce153095e04831d
Brian Cohn <brian.cohn@usc.edu>
2019-01-19 23:47:42 -0500

    working summary

---
+++ op1/Assets/changeColorOnEnter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class changeColorOnEnter : MonoBehaviour
+{
+    private Color pre = Color.red;
+    private Color post = Color.green;
+    Material m_Material;
+    void Start()
+    {
+        m_Material = GetComponent<Renderer>().material;
+        m_Material.color = pre;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("entered");
+        m_Material.color = post;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("exited");
+    }
+
+}
