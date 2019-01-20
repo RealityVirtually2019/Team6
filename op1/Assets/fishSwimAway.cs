0b2d421925b47b1025d589e3325b75c713902579
Brian Cohn <brian.cohn@usc.edu>
2019-01-20 00:03:39 -0500

    end midnight commit

---
+++ op1/Assets/fishSwimAway.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fishSwimAway : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject fishTask;
+    private Vector3 pos;
+    private Vector3 startingPos = Vector3.one;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time < 3.0 && startingPos == Vector3.one)
+        {
+            Debug.Log("got starting pos");
+            startingPos = fishTask.transform.position;
+        }
+        else if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("swimming");
+            pos = fishTask.transform.position;
+            fishTask.transform.position = new Vector3(pos.x + 0.1f, pos.y,pos.z);
+        }
+        else if (Input.GetKeyDown(KeyCode.G));
+        {
+            Debug.Log("swimmingEnd");
+            fishTask.transform.position = startingPos;
+        }
+
+    }
+}
---
+++ op1/Assets/resizeWithLeftController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resizeWithLeftController : MonoBehaviour
+{
+    public GameObject task;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            task.transform.localScale += new Vector3(0.1F, 0.1f, 0.1f);
+        } else if (Input.GetKeyDown(KeyCode.S))
+        {
+            task.transform.localScale -= new Vector3(0.1F, 0.1f, 0.1f);
+        }
+    }
+}
